0fb3ea3266f72ddfcd832e332541da4a4fd72cce
Pawel Golinski <golpaw1@gmail.com>
2017-11-08 15:33:33 +0100

    Add Agent placeholder
    It should see Redis already

--- src/pl/edu/mimuw/cloudatlas/agent/Agent.java
+++ src/pl/edu/mimuw/cloudatlas/agent/Agent.java
@@ -3,6 +3,7 @@ package pl.edu.mimuw.cloudatlas.agent;
 
 public class Agent {
     public static void main(String[] args) {
-        System.out.println("Hello, Agent!");
+        RedisKeyListener rkl = new RedisKeyListener("cpu_load");
+        rkl.getWorker().start();
     }
 }
